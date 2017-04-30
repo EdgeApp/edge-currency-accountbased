efe98143eba7db4da4dc0798456c11c055b13393
Paul Puey <paul@airbitz.co>
2017-04-30 12:48:18 -0700

    Implement makeSpend, signTx, broadcastTx, and getTransactions

--- src/abcWalletTxLib-TRD.js
+++ src/abcWalletTxLib-TRD.js
@@ -42,12 +42,16 @@ function fetchGet (cmd, params) {
     method: 'get'
   })
 }
-// function fetchPost (cmd, body) {
-//   return window.fetch(baseUrl + cmd, {
-//     method: 'post',
-//     body: JSON.stringify(body)
-//   })
-// }
+function fetchPost (cmd, body) {
+  return window.fetch(baseUrl + cmd, {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: 'post',
+    body: JSON.stringify(body)
+  })
+}
 
 class WalletLocalData {
   constructor (jsonString) {
@@ -104,10 +108,10 @@ class ABCTxLibTRD {
         fetchGet('height', '').then(function (response) {
           return response.json()
         }).then((jsonObj) => {
-          if (this.blockHeight != jsonObj.height) {
-            this.blockHeight = jsonObj.height
-            console.log('Block height changed: ' + this.blockHeight)
-            this.abcTxLibCallbacks.blockHeightChanged(this.blockHeight)
+          if (this.walletLocalData.blockHeight != jsonObj.height) {
+            this.walletLocalData.blockHeight = jsonObj.height
+            console.log('Block height changed: ' + this.walletLocalData.blockHeight)
+            this.abcTxLibCallbacks.blockHeightChanged(this.walletLocalData.blockHeight)
           }
           resolve()
         }).catch(function (err) {
@@ -174,7 +178,7 @@ class ABCTxLibTRD {
       for (var n in inputs) {
         const input = inputs[n]
         const addr = input.address
-        const idx = this.walletLocalData.addressArray.indexOf(addr)
+        const idx = this.findAddress(addr)
         if (idx != -1) {
           spendAmount += input.amount
         }
@@ -185,7 +189,7 @@ class ABCTxLibTRD {
       for (var n in outputs) {
         const output = outputs[n]
         const addr = output.address
-        const idx = this.walletLocalData.addressArray.indexOf(addr)
+        const idx = this.findAddress(addr)
         if (idx != -1) {
           receiveAmount += output.amount
         }
@@ -238,9 +242,9 @@ class ABCTxLibTRD {
         promiseArray.push(p)
 
         if (this.walletLocalData.addressArray[n] == undefined) {
-          this.walletLocalData.addressArray[n] = address
+          this.walletLocalData.addressArray[n] = { address }
         } else {
-          if (this.walletLocalData.addressArray[n] != address) {
+          if (this.walletLocalData.addressArray[n].address != address) {
             throw new Error('Derived address mismatch on index ' + n)
           }
         }
@@ -292,28 +296,32 @@ class ABCTxLibTRD {
       console.log('processAddressFromServer: response.json():')
       console.log(jsonObj)
       const txids = jsonObj.txids
+      const idx = this.findAddress(jsonObj.address)
+      if (idx === -1) {
+        throw new Error('Queried address not found in addressArray:' + jsonObj.address)
+      }
+      this.walletLocalData.addressArray[idx] = jsonObj
+
       // Iterate over txids in address
       for (var n in txids) {
-        const txid = txids[n]
+        // This address has transactions
+        const txid = txids[ n ]
         console.log('processAddressFromServer: txid:' + txid)
 
         if (this.findTransaction(txid) == -1 &&
-            this.walletLocalData.transactionsToFetch.indexOf(txid) == -1) {
+          this.walletLocalData.transactionsToFetch.indexOf(txid) == -1) {
           console.log('processAddressFromServer: txid not found. Adding:' + txid)
           this.walletLocalData.transactionsToFetch.push(txid)
           this.transactionsDirty = true
         }
 
-        const idx = this.walletLocalData.addressArray.indexOf(jsonObj.address)
-        if (idx === -1) {
-          throw new Error('Queried address not found in addressArray:' + jsonObj.address)
-        } else {
-          if (idx >= this.walletLocalData.unusedAddressIndex) {
-            this.walletLocalData.unusedAddressIndex = idx + 1
-            console.log('processAddressFromServer: set unusedAddressIndex:' + this.walletLocalData.unusedAddressIndex)
-          }
+        // Since this address is "used", make sure the unusedAddressIndex is incremented if needed
+        if (idx >= this.walletLocalData.unusedAddressIndex) {
+          this.walletLocalData.unusedAddressIndex = idx + 1
+          console.log('processAddressFromServer: set unusedAddressIndex:' + this.walletLocalData.unusedAddressIndex)
         }
       }
+
       return jsonObj.balance
     }).catch(function (err) {
       console.log('Error fetching address: ' + address)
@@ -325,6 +333,12 @@ class ABCTxLibTRD {
   findTransaction(txid) {
     return this.walletLocalData.transactionsArray.findIndex((element => {
       return element.txid == txid
+    }))
+  }
+
+  findAddress(address) {
+    return this.walletLocalData.addressArray.findIndex((element => {
+      return element.address == address
     }))
   }
 
@@ -415,7 +429,39 @@ class ABCTxLibTRD {
 
   // asynchronous
   getTransactions (options = {}) {
-    // return Promise.resolve(dataStore.getTransactions(options = {}))
+    const prom = new Promise((resolve, reject) => {
+      let startIndex = 0
+      let numEntries = 0
+      if (options == null) {
+        resolve (this.walletLocalData.transactionsArray.slice(0))
+        return
+      }
+      if (options.startIndex != undefined && options.startIndex > 0) {
+        startIndex = options.startIndex
+        if (startIndex >= this.walletLocalData.transactionsArray.length) {
+          startIndex = this.walletLocalData.transactionsArray.length - 1
+        }
+      }
+      if (options.numEntries != undefined && options.numEntries > 0) {
+        numEntries = options.numEntries
+        if (numEntries + startIndex > this.walletLocalData.transactionsArray.length) {
+          // Don't read past the end of the transactionsArray
+          numEntries = this.walletLocalData.transactionsArray.length - startIndex
+        }
+      }
+
+      // Copy the appropriate entries from the arrayTransactions
+      let returnArray = []
+
+      if (numEntries) {
+        returnArray = this.walletLocalData.transactionsArray.slice(startIndex, numEntries + startIndex)
+      } else {
+        returnArray = this.walletLocalData.transactionsArray.slice(startIndex)
+      }
+      resolve(returnArray)
+    })
+
+    return prom
   }
 
   // synchronous
@@ -437,7 +483,7 @@ class ABCTxLibTRD {
 
   // synchronous
   isAddressUsed (address, options = {}) {
-    const idx = this.walletLocalData.addressArray.indexOf(address)
+    const idx = this.findAddress(address)
     if (idx != -1) {
       const addrObj = this.walletLocalData.addressArray[ idx ]
       if (addrObj != undefined) {
@@ -451,21 +497,134 @@ class ABCTxLibTRD {
 
   // synchronous
   makeSpend (abcSpendInfo) { // returns an ABCTransaction data structure, and checks for valid info
-    // return Promise.resolve(dataStore.makeSpend(abcSpendInfo))
+    const prom = new Promise((resolve, reject) => {
+
+      // ******************************
+      // Get the fee amount
+      let networkFee = 50000
+      if (abcSpendInfo.networkFeeOption == 'high') {
+        networkFee += 10000
+      } else if (abcSpendInfo.networkFeeOption == 'low') {
+        networkFee -= 10000
+      } else if (abcSpendInfo.networkFeeOption == 'custom') {
+        if (abcSpendInfo.customNetworkFee == undefined ||
+          abcSpendInfo.customNetworkFee <= 0) {
+          reject(new Error('Invalid custom fee'))
+          return
+        } else {
+          networkFee = abcSpendInfo.customNetworkFee
+        }
+      }
+
+      // ******************************
+      // Calculate the total to send
+      let totalSpend = 0
+      let outputs = []
+      const spendTargets = abcSpendInfo.spendTargets
+
+      for (var n in spendTargets) {
+        if (spendTargets[n].amountSatoshi <= 0) {
+          reject(new Error('Error: invalid spendTarget amount'))
+          return
+        }
+        totalSpend += spendTargets[n].amountSatoshi
+        outputs.push({
+          address: spendTargets[n].publicAddress,
+          amount: spendTargets[n].amountSatoshi
+        })
+      }
+      totalSpend += networkFee
+
+      if (totalSpend > this.walletLocalData.totalBalance) {
+        reject(new Error('Error: insufficient balance'))
+        return
+      }
+
+      // ****************************************************
+      // Pick inputs. Picker will use all funds in an address
+      let totalInputAmount = 0
+      let inputs = []
+      const addressArray = this.walletLocalData.addressArray
+      for (let n in addressArray) {
+        if (addressArray[n].balance > 0) {
+          totalInputAmount += addressArray[n].balance
+          inputs.push({
+            address: addressArray[n].address,
+            amount: addressArray[n].balance
+          })
+        }
+        if (totalInputAmount >= totalSpend) {
+          break
+        }
+      }
+
+      if (totalInputAmount < totalSpend) {
+        reject(new Error('Error: insufficient funds'))
+        return
+      }
+
+      // Get a new address for change if needed
+      let changeAddress = ''
+      if (totalInputAmount > totalSpend) {
+        changeAddress = this.addressFromIndex(this.walletLocalData.unusedAddressIndex)
+        outputs.push({
+          address: changeAddress,
+          amount: (totalInputAmount - totalSpend)
+        })
+      }
+
+      // **********************************
+      // Create the unsigned ABCTransaction
+
+      let abcTransaction = {
+        amountSatoshi: totalSpend,
+        networkFee,
+        signedTx: null,
+        otherParams: {
+          inputs,
+          outputs
+        }
+      }
+
+      resolve(abcTransaction)
+
+    })
+    return prom
   }
 
   // asynchronous
   signTx (abcTransaction) {
-    // return Promise.resolve(dataStore.signTx(abcTransaction))
+    const prom = new Promise((resolve, reject) => {
+      abcTransaction.signedTx = 'iwassignedjusttrustme'
+      resolve(abcTransaction)
+    })
+
+    return prom
   }
 
   // asynchronous
   broadcastTx (abcTransaction) {
-    return Promise.resolve(true)
+    const prom = new Promise((resolve, reject) => {
+      fetchPost('spend', abcTransaction.otherParams).then(function (response) {
+        return response.json()
+      }).then((jsonObj) => {
+        // Copy params from returned transaction object to our abcTransaction object
+        abcTransaction.blockHeight = jsonObj.blockHeight
+        abcTransaction.date = jsonObj.txDate
+        resolve(abcTransaction)
+      }).catch(err => {
+        reject(new Error('Error: broadcastTx failed'))
+      })
+    })
+    return prom
   }
 
   // asynchronous
   saveTx (abcTransaction) {
-    return Promise.resolve(true)
+    const prom = new Promise((resolve, reject) => {
+      resolve(abcTransaction)
+    })
+
+    return prom
   }
 }
