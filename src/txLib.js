a12745309fccf8713cd030b7459a31402306a7af
Paul Puey <paul@airbitz.co>
2017-07-28 22:38:41 -0700

    Do not add "0x" to the start of private keys
    Maintains compatibility with current SDK app partners

--- src/txLib.js
+++ src/txLib.js
@@ -130,7 +130,7 @@ function makeEthereumPlugin (opts:any) {
         ethWallet.overrideCrypto(cryptoObj)
 
         let wallet = ethWallet.generate(false)
-        const ethereumKey = wallet.getPrivateKeyString()
+        const ethereumKey = wallet.getPrivateKeyString().replace('0x', '')
         const ethereumPublicAddress = wallet.getAddressString()
         // const ethereumKey = '0x389b07b3466eed587d6bdae09a3613611de9add2635432d6cd1521af7bbc3757'
         // const ethereumPublicAddress = '0x9fa817e5A48DD1adcA7BEc59aa6E3B1F5C4BeA9a'
