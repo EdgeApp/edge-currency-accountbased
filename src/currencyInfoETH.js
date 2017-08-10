75c86998172ed336dbe58bb5540d6b99c704233d
Paul Puey <paul@airbitz.co>
2017-08-10 12:17:28 -0700

    Use real native amounts in currencyInfo
    Also fix tests

--- src/currencyInfoETH.js
+++ src/currencyInfoETH.js
@@ -14,7 +14,7 @@ export const txLibInfo = {
       // An array of Objects of the possible denominations for this currency
       {
         name: 'ETH',
-        multiplier: 100000000,
+        multiplier: '1000000000000000000',
         symbol: 'Ξ'
       }
     ],
@@ -27,7 +27,7 @@ export const txLibInfo = {
         denominations: [
           {
             name: 'REP',
-            multiplier: 100000000
+            multiplier: '1000000000000000000'
           }
         ],
         contractAddress: '0xE94327D07Fc17907b4DB788E5aDf2ed424adDff6',
@@ -39,21 +39,10 @@ export const txLibInfo = {
         denominations: [
           {
             name: 'WINGS',
-            multiplier: 100000000
+            multiplier: '1000000000000000000'
           }
         ],
         contractAddress: '0x667088b212ce3d06a1b553a7221E1fD19000d9aF'
-      },
-      {
-        currencyCode: 'LUN',
-        currencyName: 'Lunyr',
-        denominations: [
-          {
-            name: 'LUN',
-            multiplier: 100000000
-          }
-        ],
-        contractAddress: '0xfa05a73ffe78ef8f1a739473e462c54bae6567d9'
       }
     ]
   }
