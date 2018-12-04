cf11f74d51900526b9e7d3f34b2eb92e33528125
Paul Puey <paul@edge.app>
2018-12-03 23:28:55 -0500

    Add ethereum dependencies and webpack build for them

---
+++ src/common/export-fixes.js
@@ -0,0 +1,8 @@
+/**
+ * Created by paul on 7/7/17.
+ */
+
+exports.Wallet = require('ethereumjs-wallet')
+exports.Transaction = require('ethereumjs-tx')
+exports.ABI = require('ethereumjs-abi')
+exports.Util = require('ethereumjs-util')
