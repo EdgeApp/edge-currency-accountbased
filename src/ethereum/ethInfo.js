904424073b11decec258381ed30bdcb0000e7fdc
Paul Puey <paul@edge.app>
2019-01-06 00:44:13 -0800

    Update ETH currencyInfo with new logos and and DAI

--- src/ethereum/ethInfo.js
+++ src/ethereum/ethInfo.js
@@ -43,8 +43,8 @@ export const currencyInfo: EdgeCurrencyInfo = {
       symbol: 'mΞ'
     }
   ],
-  symbolImage: `${imageServerUrl}/ethereum-logo-64.png`,
-  symbolImageDarkMono: `${imageServerUrl}/ethereum-logo-grey-64.png`,
+  symbolImage: `${imageServerUrl}/ethereum-logo-solo-64.png`,
+  symbolImageDarkMono: `${imageServerUrl}/ethereum-logo-solo-64.png`,
   metaTokens: [
     // Array of objects describing the supported metatokens
     {
@@ -57,7 +57,19 @@ export const currencyInfo: EdgeCurrencyInfo = {
         }
       ],
       contractAddress: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
-      symbolImage: `${imageServerUrl}/augur-logo-64.png`
+      symbolImage: `${imageServerUrl}/augur-logo-solo-64.png`
+    },
+    {
+      currencyCode: 'DAI',
+      currencyName: 'Dai Stablecoin',
+      denominations: [
+        {
+          name: 'DAI',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+      symbolImage: `${imageServerUrl}/dai-logo-solo-64.png`
     },
     {
       currencyCode: 'WINGS',
@@ -69,7 +81,7 @@ export const currencyInfo: EdgeCurrencyInfo = {
         }
       ],
       contractAddress: '0x667088b212ce3d06a1b553a7221E1fD19000d9aF',
-      symbolImage: `${imageServerUrl}/wings-logo-color-64.png`
+      symbolImage: `${imageServerUrl}/wings-logo-solo-64.png`
     },
     {
       currencyCode: 'USDT',
@@ -81,7 +93,7 @@ export const currencyInfo: EdgeCurrencyInfo = {
         }
       ],
       contractAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7',
-      symbolImage: `${imageServerUrl}/usdt-logo-color-64.png`
+      symbolImage: `${imageServerUrl}/tether-logo-solo-64.png`
     },
     {
       currencyCode: 'IND',
@@ -93,7 +105,7 @@ export const currencyInfo: EdgeCurrencyInfo = {
         }
       ],
       contractAddress: '0xf8e386EDa857484f5a12e4B5DAa9984E06E73705',
-      symbolImage: `${imageServerUrl}/ind-logo-64.png`
+      symbolImage: `${imageServerUrl}/indorse-logo-solo-64.png`
     },
     {
       currencyCode: 'HUR',
@@ -105,7 +117,7 @@ export const currencyInfo: EdgeCurrencyInfo = {
         }
       ],
       contractAddress: '0xCDB7eCFd3403Eef3882c65B761ef9B5054890a47',
-      symbolImage: `${imageServerUrl}/hur-logo-color-64.png`
+      symbolImage: `${imageServerUrl}/hur-logo-solo-64.png`
     }
   ]
 }
