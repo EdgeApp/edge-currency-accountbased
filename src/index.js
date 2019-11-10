0ed0e9b31707277bc7ac2760be6fc58716a5295d
Marcel Morales <morales.marcelSF@gmail.com>
2019-11-11 04:19:50 +0800

    added makeTronPlugin

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import { makeRskPlugin } from './rsk/rskPlugin.js'
 import { makeStellarPlugin } from './stellar/stellarPlugin.js'
 import { makeTezosPlugin } from './tezos/tezosPlugin.js'
 import { makeRipplePlugin } from './xrp/xrpPlugin.js'
+import { makeTronPlugin } from './tron/tronPlugin.js'
 
 const plugins = {
   eos: makeEosPlugin,
@@ -23,7 +24,8 @@ const plugins = {
   stellar: makeStellarPlugin,
   tezos: makeTezosPlugin,
   rsk: makeRskPlugin,
-  binance: makeBinancePlugin
+  binance: makeBinancePlugin,
+  tron: makeTronPlugin
 }
 
 if (
