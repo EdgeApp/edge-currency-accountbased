8108f17e81f1eef35b959b6d48a56bbd97155c77
Marcel Morales <morales.marcelsf@gmail.com>
2020-03-23 08:22:40 +0100

    added NetworkFeesSchema

--- src/tron/tronSchema.js
+++ src/tron/tronSchema.js
@@ -98,3 +98,39 @@ export const TxInfoSchema = {
   },
   required: ['blockNumber']
 }
+
+export const NetworkFeesSchema = {
+  type: 'array',
+  items: {
+    type: 'object',
+    properties: {
+      getMaintenanceTimeInterval: { type: 'number' },
+      getAccountUpgradeCost: { type: 'number' },
+      getCreateAccountFee: { type: 'number' },
+      getTransactionFee: { type: 'number' },
+      getAssetIssueFee: { type: 'number' },
+      getWitnessPayPerBlock: { type: 'number' },
+      getWitnessStandbyAllowance: { type: 'number' },
+      getCreateNewAccountFeeInSystemContract: { type: 'number' },
+      getCreateNewAccountBandwidthRate: { type: 'number' },
+      getAllowCreationOfContracts: { type: 'number' },
+      getRemoveThePowerOfTheGr: { type: 'number' },
+      getEnergyFee: { type: 'number' },
+      getExchangeCreateFee: { type: 'number' },
+      getMaxCpuTimeOfOneTx: { type: 'number' },
+      getAllowUpdateAccountName: { type: 'number' },
+      getAllowSameTokenName: { type: 'number' },
+      getAllowDelegateResource: { type: 'number' },
+      getTotalEnergyLimit: { type: 'number' },
+      getAllowTvmTransferTrc10: { type: 'number' },
+      getTotalEnergyCurrentLimit: { type: 'number' },
+      getAllowMultiSign: { type: 'number' },
+      getAllowAdaptiveEnergy: { type: 'number' },
+      getTotalEnergyTargetLimit: { type: 'number' },
+      getTotalEnergyAverageUsage: { type: 'number' },
+      getUpdateAccountPermissionFee: { type: 'number' },
+      getMultiSignFee: { type: 'number' }
+    },
+    required: ['getCreateAccountFee', 'getTransactionFee']
+  }
+}
