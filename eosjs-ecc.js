64cc741d6e6a537f75214aa6a911a01cb46699fc
kylanhurt <kylan.hurt@gmail.com>
2020-05-28 09:56:24 -0700

    Remove more unnecessary files

--- eosjs-ecc.js
+++
@@ -1,6 +0,0 @@
-const ecc = require('eosjs-ecc')
-
-const PRIVATE_KEY = '5KYuUHyzNqNd9gszoEQZZPmzSM478uBcTPn9Epdx9ZtuUpwLkfA'
-const publicKey = ecc.privateToPublic(PRIVATE_KEY)
-
-console.log('Derived public key is: ', publicKey)
