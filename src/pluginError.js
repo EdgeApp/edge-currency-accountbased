46d2865b0f59518f67da0afe1a243a4ad4ede654
Art Manalili <artmanalili@gmail.com>
2021-04-16 03:15:12 +0800

    Added error when xrp destination tag exceed 9 characters

---
+++ src/pluginError.js
@@ -0,0 +1,32 @@
+// @flow
+
+export const pluginErrorCodes = [400, 403, 404]
+export const pluginErrorName = {
+  XRP_ERROR: 'XrpError'
+}
+
+export class PluginError extends Error {
+  list: Array<{ field: string, message: string }>
+  labelCode: string
+  errorCode: number
+  json: any
+
+  constructor(
+    message: string,
+    name?: string,
+    code?: number,
+    labelCode?: string,
+    json?: any
+  ) {
+    super(message)
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, PluginError)
+    }
+
+    this.name = name ?? 'PluginError'
+    if (code) this.errorCode = code
+    if (labelCode) this.labelCode = labelCode
+    if (json) this.json = json
+  }
+}
