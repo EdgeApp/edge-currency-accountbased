2afa8abbf9003f29c5eb8e3f25217ec63854fe2e
Matthew <matthew@edge.app>
2021-06-21 20:25:18 -0700

    Add parent currency amount to InsufficientFundsError

--- src/ethereum/ethEngine.js
+++ src/ethereum/ethEngine.js
@@ -506,9 +506,12 @@ export class EthereumEngine extends CurrencyEngine {
       nativeAmount = bns.mul(totalTxAmount, '-1')
     } else {
       parentNetworkFee = nativeNetworkFee
-      // Check if there's enough parent currency to pay the transaction fee, and if not return the parent currency code
+      // Check if there's enough parent currency to pay the transaction fee, and if not return the parent currency code and amount
       if (bns.gt(nativeNetworkFee, nativeBalance)) {
-        throw new InsufficientFundsError(this.currencyInfo.currencyCode)
+        throw new InsufficientFundsError({
+          currencyCode: this.currencyInfo.currencyCode,
+          networkFee: nativeNetworkFee
+        })
       }
       const balanceToken = this.walletLocalData.totalBalances[currencyCode]
       if (bns.gt(nativeAmount, balanceToken)) {
