b267e06e739ea0affb3ae858779542ab4691bea9
Matthew <matthew@edge.app>
2022-02-14 11:35:31 -0800

    Add CUSD and CEUR tokens

--- src/ethereum/celoInfo.js
+++ src/ethereum/celoInfo.js
@@ -87,7 +87,30 @@ export const currencyInfo: EdgeCurrencyInfo = {
       symbol: 'CELO'
     }
   ],
-  metaTokens: []
+  metaTokens: [
+    {
+      currencyCode: 'CUSD',
+      currencyName: 'Celo Dollar',
+      denominations: [
+        {
+          name: 'CUSD',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0x765DE816845861e75A25fCA122bb6898B8B1282a'
+    },
+    {
+      currencyCode: 'CEUR',
+      currencyName: 'Celo Euro',
+      denominations: [
+        {
+          name: 'CEUR',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73'
+    }
+  ]
 }
 
 export const makeCeloPlugin = (opts: EdgeCorePluginOptions) => {
