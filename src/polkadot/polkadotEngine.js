09362e973f4670040b178bd497e394483b82e331
Matthew <matthew@edge.app>
2022-03-02 15:31:06 -0800

    Add balance, blockheight, and transaction loops

--- src/polkadot/polkadotEngine.js
+++ src/polkadot/polkadotEngine.js
@@ -1,14 +1,26 @@
 // @flow
 
+import { add, div, mul } from 'biggystring'
 import {
   type EdgeSpendInfo,
   type EdgeTransaction,
-  type EdgeWalletInfo
+  type EdgeWalletInfo,
+  type JsonObject
 } from 'edge-core-js/types'
 
 import { CurrencyEngine } from '../common/engine.js'
+import { getDenomInfo } from '../common/utils.js'
 import { PolkadotPlugin } from './polkadotPlugin.js'
-import { type PolkadotSettings } from './polkadotTypes.js'
+import {
+  type PolkadotOtherData,
+  type PolkadotSettings,
+  type SubscanTx,
+  asBalance,
+  asBlockheight,
+  asSubscanResponse,
+  asTransactions,
+  asTransfer
+} from './polkadotTypes.js'
 
 const ACCOUNT_POLL_MILLISECONDS = 5000
 const BLOCKCHAIN_POLL_MILLISECONDS = 20000
@@ -16,6 +28,7 @@ const TRANSACTION_POLL_MILLISECONDS = 3000
 
 export class PolkadotEngine extends CurrencyEngine {
   settings: PolkadotSettings
+  otherData: PolkadotOtherData
 
   constructor(
     currencyPlugin: PolkadotPlugin,
@@ -26,17 +39,149 @@ export class PolkadotEngine extends CurrencyEngine {
     this.settings = currencyPlugin.currencyInfo.defaultSettings.otherSettings
   }
 
-  async queryBalance() {}
+  async fetchSubscan(endpoint: string, body: JsonObject): Promise<JsonObject> {
+    const options = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    }
+    const response = await this.io.fetch(
+      this.settings.subscanBaseUrl + endpoint,
+      options
+    )
+    if (!response.ok) {
+      throw new Error(`Subscan ${endpoint} failed with ${response.status}`)
+    }
+    return asSubscanResponse(await response.json()).data
+  }
 
-  async queryBlockheight() {}
+  async queryBalance() {
+    try {
+      const payload = { key: this.walletInfo.keys.publicKey }
+      const response = await this.fetchSubscan('/v2/scan/search', payload)
+      const { balance } = asBalance(response).account
+      const denom = getDenomInfo(
+        this.currencyInfo,
+        this.currencyInfo.currencyCode
+      )
+      if (denom == null) return
+      const nativeBalance = div(balance, denom.multiplier)
+      this.updateBalance(this.currencyInfo.currencyCode, nativeBalance)
+    } catch (e) {
+      this.warn('queryBalance failed with error: ', e)
+    }
+  }
+
+  async queryBlockheight() {
+    try {
+      const payload = {
+        row: 1,
+        page: 0
+      }
+      const response = await this.fetchSubscan('/scan/blocks', payload)
+      const { block_num: blockheight } = asBlockheight(response).blocks[0]
+      if (blockheight > this.walletLocalData.blockHeight) {
+        this.walletLocalData.blockHeight = blockheight
+        this.walletLocalDataDirty = true
+        this.currencyEngineCallbacks.onBlockHeightChanged(
+          this.walletLocalData.blockHeight
+        )
+      }
+    } catch (e) {
+      this.warn('queryBlockheight failed with error: ', e)
+    }
+  }
 
   async queryFee() {}
 
-  processPolkadotTransaction() {}
+  processPolkadotTransaction(tx: SubscanTx) {
+    const {
+      from,
+      to,
+      success,
+      hash,
+      block_num: blockHeight,
+      block_timestamp: date,
+      module,
+      amount, // large denomination
+      fee // small denomination
+    } = tx
 
-  async queryTransactions() {}
+    // Skip unsuccessful and irrelevant transactions
+    if (!success || module !== 'balances') return
 
-  initOtherData() {}
+    const denom = getDenomInfo(
+      this.currencyInfo,
+      this.currencyInfo.currencyCode
+    )
+    if (denom == null) return
+
+    const ourReceiveAddresses = []
+
+    let nativeAmount = mul(amount, denom.multiplier)
+    if (from === this.walletInfo.keys.publicKey) {
+      nativeAmount = `-${add(amount, fee)}`
+    } else {
+      ourReceiveAddresses.push(to)
+    }
+
+    const edgeTransaction: EdgeTransaction = {
+      txid: hash,
+      date,
+      currencyCode: this.currencyInfo.currencyCode,
+      blockHeight,
+      nativeAmount: nativeAmount,
+      networkFee: fee,
+      ourReceiveAddresses,
+      signedTx: ''
+    }
+    this.addTransaction(this.currencyInfo.currencyCode, edgeTransaction)
+  }
+
+  async queryTransactions() {
+    let page = 0
+
+    while (true) {
+      const payload = {
+        row: this.settings.subscanQueryLimit,
+        page,
+        address: '1REAJ1k691g5Eqqg9gL7vvZCBG7FCCZ8zgQkZWd4va5ESih'
+      }
+      const response = await this.fetchSubscan('scan/transfers', payload)
+      const { count, transfers } = asTransactions(response)
+
+      // If we've already seen all the transfers we don't need to bother processing or page through older ones
+      if (count === this.otherData.processedTxCount) break
+
+      // Instead of an empty array, a null is returned when there are zero transfers
+      if (transfers == null) break
+
+      // Process txs (newest first)
+      transfers.forEach(tx => {
+        this.otherData.processedTxCount++
+        try {
+          this.processPolkadotTransaction(asTransfer(tx))
+        } catch (e) {
+          const hash = tx != null && typeof tx.hash === 'string' ? tx.hash : ''
+          this.warn(`Ignoring invalid transfer ${hash}`)
+        }
+      })
+
+      // We've reached the end of the query
+      if (transfers.length < this.settings.subscanQueryLimit) break
+
+      page++
+    }
+  }
+
+  initOtherData() {
+    if (this.otherData.processedTxCount == null) {
+      this.otherData.processedTxCount = 0
+    }
+  }
 
   // // ****************************************************************************
   // // Public methods
--- src/polkadot/polkadotTypes.js
+++ src/polkadot/polkadotTypes.js
@@ -1,3 +1,62 @@
 // @flow
 
-export type PolkadotSettings = {}
+import {
+  asArray,
+  asBoolean,
+  asEither,
+  asNull,
+  asNumber,
+  asObject,
+  asString,
+  asTuple,
+  asUnknown
+} from 'cleaners'
+
+export type PolkadotSettings = {
+  subscanBaseUrl: string,
+  subscanQueryLimit: number
+}
+
+export type PolkadotOtherData = {
+  processedTxCount: number
+}
+
+export const asSubscanResponse = asObject({
+  data: asObject(asUnknown)
+})
+
+// https://docs.api.subscan.io/#v2-api
+export const asBalance = asObject({
+  account: asObject({
+    balance: asString
+  })
+})
+
+// https://docs.api.subscan.io/#blocks
+export const asBlockheight = asObject({
+  blocks: asTuple(
+    asObject({
+      block_num: asNumber
+    })
+  )
+})
+
+// https://docs.api.subscan.io/#transfers
+export const asTransfer = asObject({
+  from: asString,
+  to: asString,
+  success: asBoolean,
+  hash: asString,
+  block_num: asNumber,
+  block_timestamp: asNumber,
+  module: asString,
+  amount: asString,
+  fee: asString
+})
+
+export type SubscanTx = $Call<typeof asTransfer>
+
+export const asTransactions = asObject({
+  count: asNumber,
+  transfers: asEither(asArray(asUnknown), asNull)
+})
