46294f2c94f1f20387b3e7496513d3ac6d6fa489
Samuel Holmes <sam@edge.app>
2022-05-23 15:58:48 -0700

    Add ropsten plugin

--- src/ethereum/ethInfos.js
+++ src/ethereum/ethInfos.js
@@ -8,6 +8,7 @@ import { makeEthereumPlugin } from './info/ethInfo.js'
 import { makeFantomPlugin } from './info/ftmInfo.js'
 import { makeKovanPlugin } from './info/kovanInfo.js'
 import { makePolygonPlugin } from './info/maticInfo'
+import { makeRopstenPlugin } from './info/ropstenInfo.js'
 import { makeRskPlugin } from './info/rskInfo.js'
 
 export const ethPlugins = {
@@ -16,6 +17,7 @@ export const ethPlugins = {
   ethereumclassic: makeEthereumClassicPlugin,
   fantom: makeFantomPlugin,
   kovan: makeKovanPlugin,
+  ropsten: makeRopstenPlugin,
   rsk: makeRskPlugin,
   polygon: makePolygonPlugin,
   celo: makeCeloPlugin,
