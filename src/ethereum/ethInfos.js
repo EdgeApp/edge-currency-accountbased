5e2e0e99bdbae888d7413d9cf27b8f9a414751f9
Samuel Holmes <sam@edge.app>
2022-05-23 12:03:48 -0700

    Add rinkeby plugin

--- src/ethereum/ethInfos.js
+++ src/ethereum/ethInfos.js
@@ -8,6 +8,7 @@ import { makeEthereumPlugin } from './info/ethInfo.js'
 import { makeFantomPlugin } from './info/ftmInfo.js'
 import { makeKovanPlugin } from './info/kovanInfo.js'
 import { makePolygonPlugin } from './info/maticInfo'
+import { makeRinkebyPlugin } from './info/rinkebyInfo.js'
 import { makeRopstenPlugin } from './info/ropstenInfo.js'
 import { makeRskPlugin } from './info/rskInfo.js'
 
@@ -17,6 +18,7 @@ export const ethPlugins = {
   ethereumclassic: makeEthereumClassicPlugin,
   fantom: makeFantomPlugin,
   kovan: makeKovanPlugin,
+  rinkeby: makeRinkebyPlugin,
   ropsten: makeRopstenPlugin,
   rsk: makeRskPlugin,
   polygon: makePolygonPlugin,
