7694be12b0db5036b3f0e94334e319dac9756196
Matthew <matthew@edge.app>
2022-07-07 12:48:40 -0700

    Add balance, blockheight, and transactions loops

--- src/polkadot/polkadotEngine.js
+++ src/polkadot/polkadotEngine.js
@@ -1,16 +1,27 @@
 // @flow
 
-import { gt, sub } from 'biggystring'
+import { add, gt, mul, sub } from 'biggystring'
 import {
   type EdgeSpendInfo,
   type EdgeTransaction,
   type EdgeWalletInfo,
+  type JsonObject,
   InsufficientFundsError
 } from 'edge-core-js/types'
 
 import { CurrencyEngine } from '../common/engine.js'
+import { getDenomInfo } from '../common/utils.js'
 import { PolkadotPlugin } from './polkadotPlugin.js'
-import { type PolkadotSettings } from './polkadotTypes.js'
+import {
+  type PolkadotSettings,
+  type SdkBalance,
+  type SdkBlockHeight,
+  type SubscanResponse,
+  type SubscanTx,
+  asSubscanResponse,
+  asTransactions,
+  asTransfer
+} from './polkadotTypes.js'
 import { ApiPromise } from './polkadotUtils'
 
 const ACCOUNT_POLL_MILLISECONDS = 5000
@@ -20,6 +31,7 @@ const TRANSACTION_POLL_MILLISECONDS = 3000
 export class PolkadotEngine extends CurrencyEngine<PolkadotPlugin> {
   settings: PolkadotSettings
   api: ApiPromise
+  nonce: number
 
   constructor(
     currencyPlugin: PolkadotPlugin,
@@ -28,17 +40,171 @@ export class PolkadotEngine extends CurrencyEngine<PolkadotPlugin> {
   ) {
     super(currencyPlugin, walletInfo, opts)
     this.settings = currencyPlugin.currencyInfo.defaultSettings.otherSettings
+    this.nonce = 0
   }
 
-  async queryBalance() {}
+  async fetchSubscan(
+    endpoint: string,
+    body: JsonObject
+  ): Promise<SubscanResponse> {
+    const options = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    }
+    const response = await this.io.fetch(
+      this.settings.subscanBaseUrl + endpoint,
+      options
+    )
+    if (!response.ok || response.status === 429) {
+      throw new Error(`Subscan ${endpoint} failed with ${response.status}`)
+    }
+    const out = await response.json()
+    return asSubscanResponse(out)
+  }
 
-  async queryBlockheight() {}
+  async queryBalance() {
+    try {
+      const response: SdkBalance = await this.api.query.system.account(
+        this.walletInfo.keys.publicKey
+      )
+      this.nonce = response.nonce
+      this.updateBalance(
+        this.currencyInfo.currencyCode,
+        response.data.free.toString()
+      )
+    } catch (e) {
+      this.warn('queryBalance failed with error: ', e)
+    }
+  }
 
-  processPolkadotTransaction(tx: SubscanTx) {}
+  async queryBlockheight() {
+    try {
+      const response: SdkBlockHeight = await this.api.rpc.chain.getBlock()
+      const height = response.block.header.number
+      if (height > this.walletLocalData.blockHeight) {
+        this.walletLocalData.blockHeight = height
+        this.walletLocalDataDirty = true
+        this.currencyEngineCallbacks.onBlockHeightChanged(
+          this.walletLocalData.blockHeight
+        )
+      }
+    } catch (e) {
+      this.warn('queryBlockheight failed with error: ', e)
+    }
+  }
 
-  async queryTransactions() {}
+  processPolkadotTransaction(tx: SubscanTx) {
+    const {
+      from,
+      to,
+      success,
+      hash,
+      block_num: blockHeight,
+      block_timestamp: date,
+      module,
+      amount, // large denomination
+      fee // small denomination
+    } = tx
 
-  initOtherData() {}
+    // Skip unsuccessful and irrelevant transactions
+    if (!success || module !== 'balances') return
+
+    const denom = getDenomInfo(
+      this.currencyInfo,
+      this.currencyInfo.currencyCode
+    )
+    if (denom == null) return
+
+    const ourReceiveAddresses = []
+
+    let nativeAmount = mul(amount, denom.multiplier)
+    if (from === this.walletInfo.keys.publicKey) {
+      nativeAmount = `-${add(amount, fee)}`
+    } else {
+      ourReceiveAddresses.push(to)
+    }
+
+    const edgeTransaction: EdgeTransaction = {
+      txid: hash,
+      date,
+      currencyCode: this.currencyInfo.currencyCode,
+      blockHeight,
+      nativeAmount: nativeAmount,
+      networkFee: fee,
+      ourReceiveAddresses,
+      signedTx: ''
+    }
+    this.addTransaction(this.currencyInfo.currencyCode, edgeTransaction)
+  }
+
+  async queryTransactions() {
+    // Skip pages we don't need
+    let page = Math.floor(
+      this.otherData.txCount / this.settings.subscanQueryLimit
+    )
+    let numProcessedTxs = this.otherData.txCount
+
+    while (true) {
+      const payload = {
+        row: this.settings.subscanQueryLimit,
+        page,
+        address: this.walletInfo.keys.publicKey
+      }
+      const response = await this.fetchSubscan('/scan/transfers', payload)
+      const { count, transfers } = asTransactions(response.data)
+
+      // count is the total number of transactions ever for an account
+      // If we've already seen all the transfers we don't need to bother processing or page through older ones
+      if (count === this.otherData.txCount) break
+
+      // Instead of an empty array, a null is returned when there are zero transfers
+      if (transfers == null) break
+
+      // Process txs (newest first)
+      transfers.forEach(tx => {
+        try {
+          this.processPolkadotTransaction(asTransfer(tx))
+        } catch (e) {
+          const hash = tx != null && typeof tx.hash === 'string' ? tx.hash : ''
+          this.warn(`Ignoring invalid transfer ${hash}`)
+        }
+        numProcessedTxs++
+      })
+
+      // We've reached the end of the query
+      if (transfers.length < this.settings.subscanQueryLimit) break
+
+      page++
+      this.tokenCheckTransactionsStatus[this.currencyInfo.currencyCode] =
+        numProcessedTxs / count
+      this.updateOnAddressesChecked()
+    }
+
+    if (this.otherData.txCount < numProcessedTxs) {
+      this.otherData.txCount = numProcessedTxs
+      this.walletLocalDataDirty = true
+    }
+
+    this.tokenCheckTransactionsStatus[this.currencyInfo.currencyCode] = 1
+    this.updateOnAddressesChecked()
+
+    if (this.transactionsChangedArray.length > 0) {
+      this.currencyEngineCallbacks.onTransactionsChanged(
+        this.transactionsChangedArray
+      )
+      this.transactionsChangedArray = []
+    }
+  }
+
+  initOtherData() {
+    if (this.otherData.txCount == null) {
+      this.otherData.txCount = 0
+    }
+  }
 
   // // ****************************************************************************
   // // Public methods
