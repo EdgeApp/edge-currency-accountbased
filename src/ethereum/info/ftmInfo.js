1d8d7cfa2a2f904d50ad1378087aefccb50f1e45
Jon Tzeng <jon@edge.app>
2022-07-12 18:31:08 -0700

    Add new Cemetery-V2 tokens

--- src/ethereum/info/ftmInfo.js
+++ src/ethereum/info/ftmInfo.js
@@ -90,6 +90,28 @@ export const currencyInfo: EdgeCurrencyInfo = {
   metaTokens: [
     // Array of objects describing the supported metatokens
     {
+      currencyCode: 'AVAX',
+      currencyName: 'Avalanche',
+      denominations: [
+        {
+          name: 'AVAX',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0x511d35c52a3c244e7b8bd92c0c297755fbd89212'
+    },
+    {
+      currencyCode: 'BNB',
+      currencyName: 'Binance',
+      denominations: [
+        {
+          name: 'BNB',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0xd67de0e0a0fd7b15dc8348bb9be742f3c5850454'
+    },
+    {
       currencyCode: 'BOO',
       currencyName: 'SpookyToken',
       denominations: [
@@ -99,6 +121,50 @@ export const currencyInfo: EdgeCurrencyInfo = {
         }
       ],
       contractAddress: '0x841fad6eae12c286d1fd18d1d525dffa75c7effe'
+    },
+    {
+      currencyCode: 'BTC',
+      currencyName: 'Bitcoin',
+      denominations: [
+        {
+          name: 'BTC',
+          multiplier: '100000000'
+        }
+      ],
+      contractAddress: '0x321162cd933e2be498cd2267a90534a804051b11'
+    },
+    {
+      currencyCode: 'CRV',
+      currencyName: 'Curve',
+      denominations: [
+        {
+          name: 'CRV',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0x1e4f97b9f9f913c46f1632781732927b9019c68b'
+    },
+    {
+      currencyCode: 'DAI',
+      currencyName: 'DAI Stablecoin',
+      denominations: [
+        {
+          name: 'DAI',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e'
+    },
+    {
+      currencyCode: 'ETH',
+      currencyName: 'Ethereum',
+      denominations: [
+        {
+          name: 'ETH',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0x74b23882a30290451a17c44f4f05243b6b58c76d'
     },
     {
       currencyCode: 'FBTC',
@@ -123,6 +189,17 @@ export const currencyInfo: EdgeCurrencyInfo = {
       contractAddress: '0x658b0c7613e890ee50b8c4bc6a3f41ef411208ad'
     },
     {
+      currencyCode: 'FUSD',
+      currencyName: 'Fantom USD',
+      denominations: [
+        {
+          name: 'FUSD',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0xad84341756bf337f5a0164515b1f6f993d194e1f'
+    },
+    {
       currencyCode: 'FUSDT',
       currencyName: 'Frapped Tether',
       denominations: [
@@ -134,6 +211,39 @@ export const currencyInfo: EdgeCurrencyInfo = {
       contractAddress: '0x049d68029688eabf473097a2fc38ef61633a3c7a'
     },
     {
+      currencyCode: 'LIF3',
+      currencyName: 'LIF3',
+      denominations: [
+        {
+          name: 'LIF3',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0xbf60e7414ef09026733c1e7de72e7393888c64da'
+    },
+    {
+      currencyCode: 'LINK',
+      currencyName: 'Chainlink',
+      denominations: [
+        {
+          name: 'LINK',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0xb3654dc3d10ea7645f8319668e8f54d2574fbdc8'
+    },
+    {
+      currencyCode: 'LSHARE',
+      currencyName: 'LIF3 LSHARE',
+      denominations: [
+        {
+          name: 'LSHARE',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0xcbe0ca46399af916784cadf5bcc3aed2052d6c45'
+    },
+    {
       currencyCode: 'MAI',
       currencyName: 'miMATIC',
       denominations: [
@@ -143,6 +253,17 @@ export const currencyInfo: EdgeCurrencyInfo = {
         }
       ],
       contractAddress: '0xfB98B335551a418cD0737375a2ea0ded62Ea213b'
+    },
+    {
+      currencyCode: 'MIM',
+      currencyName: 'Magic Internet Money',
+      denominations: [
+        {
+          name: 'MIM',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0x82f0b8b456c1a451378467398982d4834b6829c1'
     },
     {
       currencyCode: 'TBOND',
@@ -165,6 +286,17 @@ export const currencyInfo: EdgeCurrencyInfo = {
         }
       ],
       contractAddress: '0x6c021Ae822BEa943b2E66552bDe1D2696a53fbB7'
+    },
+    {
+      currencyCode: 'TREEB',
+      currencyName: 'Retreeb',
+      denominations: [
+        {
+          name: 'TREEB',
+          multiplier: '1000000000000000000'
+        }
+      ],
+      contractAddress: '0xc60d7067dfbc6f2caf30523a064f416a5af52963'
     },
     {
       currencyCode: 'TSHARE',
@@ -209,6 +341,17 @@ export const currencyInfo: EdgeCurrencyInfo = {
         }
       ],
       contractAddress: '0xa48d959AE2E88f1dAA7D5F611E01908106dE7598'
+    },
+    {
+      currencyCode: 'ZOO',
+      currencyName: 'Zookeeper',
+      denominations: [
+        {
+          name: 'ZOO',
+          multiplier: '1'
+        }
+      ],
+      contractAddress: '0x09e145a1d53c0045f41aeef25d8ff982ae74dd56'
     }
   ]
 }
