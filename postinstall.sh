72b4affe2a660887b903e88265f1e51ed2bdcb2e
Matthew <matthew@edge.app>
2022-08-18 17:02:05 -0700

    Fix tests and precommit
    Replace optional usb and node-hid directories with fake files. These dependencies aren't used and don't build.
    Ignore broken json file in dependencies

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # The `usb` and 'node-hid' modules doesn't properly install on some boxes:
+rm -rf node_modules/usb
 mkdir -p node_modules/usb
 touch node_modules/usb/index.js
+rm -rf node_modules/node-hid
 mkdir -p node_modules/node-hid
 touch node_modules/node-hid/index.js
