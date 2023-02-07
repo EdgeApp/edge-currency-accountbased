58c4163c43e481f0ff63ff762e1e4f41a9d80735
Matthew <matthew@edge.app>
2023-02-06 23:53:53 -0800

    Add PowerUp

--- src/eos/info/telosInfo.ts
+++ src/eos/info/telosInfo.ts
@@ -14,6 +14,7 @@ export const telosNetworkInfo: EosNetworkInfo = {
   ],
   eosHyperionNodes: ['https://telos.caleos.io'],
   eosNodes: ['https://telos.caleos.io'],
+  powerUpServers: [],
   eosDfuseServers: [],
   uriProtocol: 'telos'
 }
