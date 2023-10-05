5d4496ffe1925371e36e5117ce25ae152b8d2124
peachbits <matthew@edge.app>
2023-10-05 16:32:47 -0700

    Return Birthday Height with private seed

--- src/zcash/ZcashTools.ts
+++ src/zcash/ZcashTools.ts
@@ -54,7 +54,7 @@ export class ZcashTools implements EdgeCurrencyTools {
   ): Promise<string> {
     const { pluginId } = this.currencyInfo
     const keys = asZcashPrivateKeys(pluginId)(privateWalletInfo.keys)
-    return keys.mnemonic
+    return `Seed Phrase:\n${keys.mnemonic}\n\nBirthday Height:\n${keys.birthdayHeight}`
   }
 
   async getDisplayPublicKey(publicWalletInfo: EdgeWalletInfo): Promise<string> {
