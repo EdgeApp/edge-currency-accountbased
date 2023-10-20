36e4b39a928802049446a2669c4a6c72fd4c0594
Matthew <matthew@edge.app>
2023-10-19 20:02:49 -0700

    Add key derivation utils

--- src/cosmos/cosmosTools.ts
+++ src/cosmos/cosmosTools.ts
@@ -1,3 +1,6 @@
+import { stringToPath } from '@cosmjs/crypto'
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing'
+import { entropyToMnemonic, validateMnemonic } from 'bip39'
 import {
   EdgeCurrencyInfo,
   EdgeCurrencyTools,
@@ -9,9 +12,14 @@ import {
   EdgeWalletInfo,
   JsonObject
 } from 'edge-core-js/types'
+import { base16 } from 'rfc4648'
 
 import { PluginEnvironment } from '../common/innerPlugin'
-import { CosmosNetworkInfo } from './cosmosTypes'
+import {
+  asCosmosPrivateKeys,
+  asSafeCosmosWalletInfo,
+  CosmosNetworkInfo
+} from './cosmosTypes'
 
 export class CosmosTools implements EdgeCurrencyTools {
   io: EdgeIo
@@ -27,26 +35,64 @@ export class CosmosTools implements EdgeCurrencyTools {
     this.networkInfo = networkInfo
   }
 
+  async createSigner(mnemonic: string): Promise<DirectSecp256k1HdWallet> {
+    const { bech32AddressPrefix, bip39Path } = this.networkInfo
+    const path = stringToPath(bip39Path)
+    const signer = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
+      hdPaths: [path],
+      prefix: bech32AddressPrefix
+    })
+    return signer
+  }
+
   async getDisplayPrivateKey(
     privateWalletInfo: EdgeWalletInfo
   ): Promise<string> {
-    throw new Error('not implemented')
+    const { pluginId } = this.currencyInfo
+    const keys = asCosmosPrivateKeys(pluginId)(privateWalletInfo.keys)
+    return keys.mnemonic
   }
 
   async getDisplayPublicKey(publicWalletInfo: EdgeWalletInfo): Promise<string> {
-    throw new Error('not implemented')
+    const { keys } = asSafeCosmosWalletInfo(publicWalletInfo)
+    return keys.publicKey
   }
 
   async importPrivateKey(input: string): Promise<JsonObject> {
-    throw new Error('not implemented')
+    const isValid = validateMnemonic(input)
+    if (!isValid) throw new Error('Invalid mnemonic')
+
+    // Test it
+    await this.createSigner(input)
+
+    return { [this.networkInfo.pluginMnemonicKeyName]: input }
   }
 
   async createPrivateKey(walletType: string): Promise<JsonObject> {
-    throw new Error('not implemented')
+    if (walletType !== this.currencyInfo.walletType) {
+      throw new Error('InvalidWalletType')
+    }
+
+    const entropy = Buffer.from(this.io.random(32))
+    const mnemonic = entropyToMnemonic(entropy)
+
+    return await this.importPrivateKey(mnemonic)
   }
 
   async derivePublicKey(walletInfo: EdgeWalletInfo): Promise<JsonObject> {
-    throw new Error('not implemented')
+    if (walletInfo.type !== this.currencyInfo.walletType) {
+      throw new Error('InvalidWalletType')
+    }
+
+    const { mnemonic } = asCosmosPrivateKeys(this.currencyInfo.pluginId)(
+      walletInfo.keys
+    )
+    const signer = await this.createSigner(mnemonic)
+    const accountInfos = await signer.getAccounts()
+    const { address, /* algo */ pubkey } = accountInfos[0]
+    const publicKey = base16.stringify(pubkey)
+
+    return { bech32Address: address, publicKey }
   }
 
   async parseUri(
--- src/cosmos/cosmosTypes.ts
+++ src/cosmos/cosmosTypes.ts
@@ -1,15 +1,51 @@
-import { asSafeCommonWalletInfo } from '../common/types'
+import { asCodec, asObject, asString, Cleaner } from 'cleaners'
 
-// eslint-disable-next-line
-export interface CosmosNetworkInfo {}
+import { asWalletInfo } from '../common/types'
+
+export interface CosmosNetworkInfo {
+  bech32AddressPrefix: string
+  bip39Path: string
+  pluginMnemonicKeyName: string
+}
 
 //
 // Wallet Info and Keys:
 //
 
 export type SafeCosmosWalletInfo = ReturnType<typeof asSafeCosmosWalletInfo>
-export const asSafeCosmosWalletInfo = asSafeCommonWalletInfo
+export const asSafeCosmosWalletInfo = asWalletInfo(
+  asObject({ bech32Address: asString, publicKey: asString })
+)
 
 export interface CosmosPrivateKeys {
   mnemonic: string
+}
+export const asCosmosPrivateKeys = (
+  pluginId: string
+): Cleaner<CosmosPrivateKeys> => {
+  // Type hacks:
+  type PluginId = 'x'
+  type FromKeys = {
+    [key in `${PluginId}Mnemonic`]: string
+  }
+  const _pluginId = pluginId as PluginId
+  // Derived cleaners from the generic parameter:
+  const asFromKeys: Cleaner<FromKeys> = asObject({
+    [`${_pluginId}Mnemonic`]: asString
+  }) as Cleaner<any>
+
+  return asCodec(
+    (value: unknown) => {
+      const from = asFromKeys(value)
+      const to: CosmosPrivateKeys = {
+        mnemonic: from[`${_pluginId}Mnemonic`]
+      }
+      return to
+    },
+    cosmosPrivateKey => {
+      return {
+        [`${_pluginId}Mnemonic`]: cosmosPrivateKey.mnemonic
+      }
+    }
+  )
 }
