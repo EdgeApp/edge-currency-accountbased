1879f3a46ddcc8b4b04083f5524559773a2067f1
Matthew <matthew@edge.app>
2024-01-02 14:08:48 -0800

    Cleanup eslint error suppression

--- src/ethereum/EthereumEngine.ts
+++ src/ethereum/EthereumEngine.ts
@@ -1160,16 +1160,14 @@ export class EthereumEngine extends CurrencyEngine<
     // Nonce:
 
     let nonce: string | undefined = otherParams.nonceUsed
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (!nonce) {
+    if (nonce == null) {
       // Use an unconfirmed nonce if
       // 1. We have unconfirmed spending txs in the transaction list
       // 2. It is greater than the confirmed nonce
       // 3. Is no more than 5 higher than confirmed nonce
       // Otherwise, use the next nonce
       if (
-        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-        this.walletLocalData.numUnconfirmedSpendTxs &&
+        this.walletLocalData.numUnconfirmedSpendTxs != null &&
         gt(this.otherData.unconfirmedNextNonce, this.otherData.nextNonce)
       ) {
         const diff = sub(
