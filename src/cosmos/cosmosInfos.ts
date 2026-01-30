fbaecccc4e6aff7d99adf3790be540df9cbb976d
Jon Tzeng <jon@edge.app>
2026-01-29 16:40:01 -0800

    Add Cacao (CACAO) MAYAChain support
    Add support for MAYAChain's native CACAO token:
    - Create MayachainEngine extending MidgardEngine (uses standard Cosmos fees)
    - Add cacaoInfo.ts with network config (10 decimals for CACAO, 4 for MAYA)
    - MAYA builtin token included

--- src/cosmos/cosmosInfos.ts
+++ src/cosmos/cosmosInfos.ts
@@ -1,6 +1,7 @@
 import { axelar } from './info/axelarInfo'
 import { coreum } from './info/coreumInfo'
 import { cosmoshub } from './info/cosmoshubInfo'
+import { mayachain } from './info/mayachainInfo'
 import { nym } from './info/nymInfo'
 import { osmosis } from './info/osmosisInfo'
 import { thorchainrune } from './info/thorchainruneInfo'
@@ -8,6 +9,7 @@ import { thorchainrunestagenet } from './info/thorchainruneStagenetInfo'
 
 export const cosmosPlugins = {
   axelar,
+  mayachain,
   coreum,
   cosmoshub,
   nym,
